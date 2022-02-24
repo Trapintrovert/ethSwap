bb71c3f3e7fd6f69b3de3657675686e559e50246
trapintrovert <mcneo062@gmail.com>
2022-02-24 12:50:51 +0100

    Final sellTokens contract

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -7,6 +7,20 @@ contract EthSwap {
    Token public token;
    uint public rate = 100;
 
+   event TokenPurchased(
+      address account,
+      address token,
+      uint amount,
+      uint rate
+   );
+
+   event TokenSold(
+      address account,
+      address token,
+      uint amount,
+      uint rate
+   );
+
    constructor(Token _token) public {
       token = _token;
    }
@@ -14,6 +28,33 @@ contract EthSwap {
    function buyTokens() public payable{
       // Calculate the nu,ber of tokens to buy
       uint tokenAmount = msg.value * rate;
+
+      //require that EthSwap has enough tokens
+      require(token.balanceOf(address(this)) >= tokenAmount);
+
+      // Transfer tokens to the user
       token.transfer(msg.sender, tokenAmount);
+
+      // Emit an event
+      emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+   }
+
+
+   function sellTokens(uint _amount) public {
+      // User can't sell more tokens than they have
+      require(token.balanceOf(msg.sender) >= _amount);
+
+      // Calculate the amount of Ether to redeem
+      uint etherAmount = _amount / rate;
+
+      // Require that EthSwap has enough Ether
+      require(address(this).balance >= etherAmount);
+
+      // Perform sell
+      token.transferFrom(msg.sender, address(this), _amount);
+      msg.sender.transfer(etherAmount);
+
+      // Emit on event
+      emit TokenSold(msg.sender, address(token), _amount, rate);
    }
 }
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -15,7 +15,7 @@ require('chai')
     }
 
 // eslint-disable-next-line no-undef
-contract('EthSwap', (accounts) => {
+contract('EthSwap', ([deployer, investor]) => {
     let token, ethSwap
 
     // eslint-disable-next-line no-undef
@@ -49,9 +49,72 @@ contract('EthSwap', (accounts) => {
         })
     })
 
-    describe('buyTokens', async() => {
+    describe('buyTokens()', async() => {
+        let result
+        // eslint-disable-next-line no-undef
+        before(async() => {
+            
+             // eslint-disable-next-line no-undef
+             result =  await ethSwap.buyTokens({from: investor, value: web3.utils.toWei('1', 'ether')})
+        })
+
         it('Allows user to instantly purchase tokens from ethSwap for a fixed price', async () => {
-            await ethSwap.buyTokens({from: accounts[1], value: '1000000000000000000'})
+           // Check investor token balance after purchase
+           let investorBalance = await token.balanceOf(investor)
+           assert.equal(investorBalance.toString(), tokens('100'))
+
+           // Check ethSwap balance after purchase
+           let ethSwapBalance
+           ethSwapBalance = await token.balanceOf(ethSwap.address)
+           assert.equal(ethSwapBalance.toString(), tokens('999900'))
+           // eslint-disable-next-line no-undef
+           ethSwapBalance = await web3.eth.getBalance(ethSwap.address)
+           // eslint-disable-next-line no-undef
+           assert.equal(ethSwapBalance.toString(), web3.utils.toWei('1', 'Ether'))
+
+
+           const event = result.logs[0].args
+           assert.equal(event.account, investor)
+           assert.equal(event.token, token.address)
+           assert.equal(event.amount.toString(), tokens('100').toString())
+           assert.equal(event.rate.toString(), '100')
+        })
+    })
+
+    describe('sellTokens()', async() => {
+        let result
+        // eslint-disable-next-line no-undef
+        before(async() => {
+            // Investor must approve tokens before the purchase
+            await token.approve(ethSwap.address, tokens('100'), {from: investor})
+            // Investor sells tokens
+            result = await ethSwap.sellTokens(tokens('100'), {from: investor})
+        })
+
+        it('Allows user to instantly sell tokens to ethSwap for a fixed price', async () => {
+             // Check investor token balance after purchase
+             let investorBalance = await token.balanceOf(investor)
+             assert.equal(investorBalance.toString(), tokens('0'))
+
+
+              // Check ethSwap balance after purchase
+            let ethSwapBalance
+            ethSwapBalance = await token.balanceOf(ethSwap.address)
+            assert.equal(ethSwapBalance.toString(), tokens('1000000'))
+            // eslint-disable-next-line no-undef
+            ethSwapBalance = await web3.eth.getBalance(ethSwap.address)
+            // eslint-disable-next-line no-undef
+            assert.equal(ethSwapBalance.toString(), web3.utils.toWei('0', 'Ether'))
+
+            // Check log to ensure event was emitted with correct data
+            const event = result.logs[0].args
+            assert.equal(event.account, investor)
+            assert.equal(event.token, token.address)
+            assert.equal(event.amount.toString(), tokens('100').toString())
+            assert.equal(event.rate.toString(), '100')
+
+            //FAILURE: investor can't sell more token than they have
+            await ethSwap.sellTokens(tokens('500'), { from:investor}).should.be.rejected;
         })
     })
 })
